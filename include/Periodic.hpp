8ab98d29ffe043767456a1bd5f78ad8b8eb16b0f
KANEKO Kojiro <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2024-04-03 14:41:32 +0900

    add: define interface for compression phase

--- include/Periodic.hpp
+++ include/Periodic.hpp
@@ -18,3 +18,16 @@ public:
     ~Periodic();
     void Run(json parameter_json, int phase_index, std::string root_directory_of_data, double time_scale);
 };
+
+class PeriodicCompression
+{
+protected:
+    Rods &rods;
+    ParametersForRods &parameter;
+
+public:
+
+    PeriodicCompression(Rods &rods, ParametersForRods &parameter_for_rods);
+    ~PeriodicCompression();
+    void Run(json parameter_json, int phase_index, std::string root_directory_of_data, double time_scale, double initial_linear_dimension, double final_linear_dimension);
+};
