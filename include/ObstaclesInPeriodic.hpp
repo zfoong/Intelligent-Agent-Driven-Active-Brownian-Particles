7a535b6b0d394f437cba5cb62d8a04c053d341e0
KANEKO Kojiro <k.kaneko@cmt.phys.kyushu-u.ac.jp>
2024-05-16 21:04:11 +0900

    add: define and implement ObstaclesInPeriodicCompression phase

--- include/ObstaclesInPeriodic.hpp
+++ include/ObstaclesInPeriodic.hpp
@@ -10,6 +10,18 @@ using json = nlohmann::json;
 #include "CircularBoundary.hpp"
 #include "Periodic.hpp"
 
+class ObstaclesInPeriodicCompression : PeriodicCompression
+{
+protected:
+    std::vector<ParametersForCircularBoundary> &parameters_for_obstacles;
+    std::vector<CircularBoundary> obstacles;
+
+public:
+    ObstaclesInPeriodicCompression(Rods &rods, ParametersForRods &parameter_for_rods, std::vector<ParametersForCircularBoundary> &parameters_for_obstacles);
+    ~ObstaclesInPeriodicCompression();
+    void Run(json parameter_json, int phase_index, std::string root_directory_of_data, double time_scale, double initial_linear_dimension, double final_linear_dimension);
+};
+
 class ObstaclesSteepeningInPeriodic
 {
 protected:
--- src/ObstaclesInPeriodic.cpp
+++ src/ObstaclesInPeriodic.cpp
@@ -9,9 +9,124 @@ using json = nlohmann::json;
 #include "ParametersForRods.hpp"
 #include "Rods.hpp"
 #include "ParametersForForces.hpp"
+#include "RodsCircularObstacleYukawaForces.hpp"
 #include "RodsCircularObstacleCappedLennardJonesForces.hpp"
 #include "ForcesOnSegments2d.hpp"
 #include "util.hpp"
+
+ObstaclesInPeriodicCompression::ObstaclesInPeriodicCompression(
+    Rods &rods,
+    ParametersForRods &parameter_for_rods,
+    std::vector<ParametersForCircularBoundary> &parameters_for_obstacles
+) : PeriodicCompression(rods, parameter_for_rods),
+    parameters_for_obstacles(parameters_for_obstacles)
+{
+    obstacles.reserve(parameters_for_obstacles.size());
+    for (size_t i = 0; i < parameters_for_obstacles.size(); i++) {
+        obstacles.emplace_back(parameters_for_obstacles.at(i), 1000);
+    }
+
+    std::cout << "-- Parameters for Rods in Compression --" << std::endl;
+    this->parameter.DisplayParameters();
+
+    std::cout << "-- Parameters for Obstacles in Compression --" << std::endl;
+    for (size_t i = 0; i < parameters_for_obstacles.size(); i++) {
+        std::cout << "Obstacle " << i << ":" << std::endl;
+        parameters_for_obstacles.at(i).displayParameters();
+    }
+}
+
+ObstaclesInPeriodicCompression::~ObstaclesInPeriodicCompression()
+{
+}
+
+void ObstaclesInPeriodicCompression::Run(json parameter_json, int phase_index, std::string root_directory_of_data, double time_scale, double initial_linear_dimension, double final_linear_dimension)
+{
+    json phase_parameter_json = parameter_json["phases"][phase_index];
+
+    const std::string phase_name = phase_parameter_json["name"].template get<std::string>();
+    const int total_steps = phase_parameter_json["total_steps"].template get<int>();
+    const int total_steps_digits = std::to_string(total_steps).length();
+    const double x_min = phase_parameter_json["x_min"].template get<double>();
+    double x_max = phase_parameter_json["x_max"].template get<double>();
+    const double y_min = phase_parameter_json["y_min"].template get<double>();
+    double y_max = phase_parameter_json["y_max"].template get<double>();
+
+    double linear_dimension = initial_linear_dimension;
+    const double ratio = std::pow(final_linear_dimension / initial_linear_dimension, 1. / total_steps);
+
+    // declare rod-rod forces
+    parameter_json["time_scaled_interaction_strength_between_rod_rod"]
+        = parameter_json["interaction_strength_between_rod_rod"].template get<double>() / parameter.num_of_segments / parameter.num_of_segments
+        * time_scale * time_scale; // interaction potential coefficient scaled by time interval per time step in simulation
+    ForcesOnSegments2d rod_rod_forces(parameter, parameter_json["time_scaled_interaction_strength_between_rod_rod"].template get<double>());
+
+    // declare rod-obstacle forces
+    parameter_json["time_scaled_interaction_strength_between_rod_obstacle"]
+        = parameter_json["interaction_strength_between_rod_obstacle"].template get<double>() / parameter.num_of_segments
+        * time_scale * time_scale; // interaction potential coefficient scaled by time interval per time step in simulation
+    std::vector<ParametersForForces> parameters_for_forces;
+    parameters_for_forces.reserve(parameters_for_obstacles.size());
+    std::vector<RodsCircularObstacleYukawaForces> rod_obstacle_forces;
+    rod_obstacle_forces.reserve(parameters_for_obstacles.size());
+    for (size_t i = 0; i < parameters_for_obstacles.size(); i++) {
+        parameters_for_forces.emplace_back(parameter_json["time_scaled_interaction_strength_between_rod_obstacle"].template get<double>(), parameter.diameter_of_segment);
+        rod_obstacle_forces.emplace_back(parameter, parameters_for_obstacles.at(i), parameters_for_forces.at(i));
+    }
+
+    std::cout << "start " << phase_name << " phase..." << std::endl;
+    for (int steps = 0; steps < total_steps; steps++) {
+        // compute rod-rod forces
+        rod_rod_forces.calcRodRodYukawaForcesWithPeriodic(rods, x_min, x_max, y_min, y_max);
+        rod_rod_forces.calcForcesOnRods2d(rods);
+        auto [fx_rod_rod, fy_rod_rod, torque_rod_rod] = rod_rod_forces.getForcesOnRods2d();
+        rod_rod_forces.ClearCalculatedForces();
+        rods.addForces(fx_rod_rod, fy_rod_rod, torque_rod_rod);
+
+        // compute rod-obstacle forces
+        for (size_t i = 0; i < rod_obstacle_forces.size(); i++) {
+            rod_obstacle_forces.at(i).compute(rods);
+            auto [fx_rod_obstacle, fy_rod_obstacle, torque_rod_obstacle] = rod_obstacle_forces.at(i).getForcesOnRods();
+            rod_obstacle_forces.at(i).clear();
+            rods.addForces(fx_rod_obstacle, fy_rod_obstacle, torque_rod_obstacle);
+        }
+
+
+        // compute velocity from total forces
+        rods.calcVelocitiesFromForces();
+
+        if ((steps % parameter_json["step_interval_for_output"].template get<int>()) == 0) {
+            rods.writeData(root_directory_of_data, phase_name, steps, total_steps_digits);
+            // output obstacle shape as discrete points for each step
+            for (size_t i = 0; i < parameters_for_obstacles.size(); i++) {
+                obstacles.at(i).writeData(root_directory_of_data, phase_name, i, steps, total_steps_digits);
+            }
+        }
+
+        if ((total_steps >= 10) && (steps % (total_steps / 10) == 0)) {  // show progress
+            DisplayProgress(steps, total_steps);
+        }
+
+        // update for next step
+        rods.updateRods();
+        rods.periodic(x_min, x_max, y_min, y_max);
+
+        // compression of system
+        linear_dimension *= ratio;
+        x_max = linear_dimension;
+        y_max = linear_dimension;
+        // compression of rods
+        rods.scalePosition(ratio);
+        // compression of obstacles
+        for (size_t i = 0; i < parameters_for_obstacles.size(); i++) {
+            auto [center_x, center_y] = parameters_for_obstacles.at(i).getPositionOfCenter();
+            obstacles.at(i).setPositionOfCenter(center_x * ratio, center_y * ratio);
+            obstacles.at(i).calcPointsOnBoundary();
+            rod_obstacle_forces.at(i).setCenterOfCircularObstacle(center_x * ratio, center_y * ratio);
+        }
+    }
+    std::cout << "finish " << phase_name << " phase!" << std::endl;
+}
 
 ObstaclesSteepeningInPeriodic::ObstaclesSteepeningInPeriodic(
     Rods &rods,
